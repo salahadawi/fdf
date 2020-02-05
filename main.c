2aea5cf71d977e002768c441a2a503317108bcc9
Salah Adawi <sadawi@student.hive.fi>
2020-02-05 14:51:07 +0200

    split functions to several files

--- main.c
+++ main.c
@@ -50,131 +50,30 @@ void	handle_error(int code)
 		exit(0);
 	}
 
-//move to libft
-int		ft_isdigit_neg(char *str)
+void	handle_graphics(t_map *s_map)
 {
-	return (str[0] == '-' && ft_isdigit(str[1]));
-}
+	t_mlx	*mlx;
 
-void	check_line(char *line)
-{
-	int i;
-
-	i = 0;
-	while (line[i])
-	{
-		if (!ft_isdigit(line[i]) && line[i] != ' ' && !ft_isdigit_neg(&line[i]))
-			handle_error(2);
-		i++;
-	}
-}
-
-int		count_ints(char *line)
-{
-	int i;
-	int count;
-
-	i = 0;
-	count = 0;
-	while (line[i])
-	{
-		while (line[i] == ' ' && line[i])
-			i++;
-		if (ft_isdigit(line[i]) || ft_isdigit_neg(&line[i]))
-		{
-			if (line[i] == '-')
-				i++;
-			while (ft_isdigit(line[i]))
-				i++;
-			count++;
-		}
-	}
-	return (count);
-}
-
-void	store_map_line(char *line, t_map *s_map)
-{
-	int i;
-	int col;
-	int count;
-
-	i = 0;
-	col = 0;
-	check_line(line);
-	count = count_ints(line);
-	if (s_map->cols)
-		if (count != s_map->cols)
-			handle_error(2);
-	s_map->map[s_map->rows] = (int*)malloc(sizeof(int) * count);
-	while (line[i])
-	{
-		while (line[i] == ' ')
-			i++;
-		if (ft_isdigit(line[i]) || ft_isdigit_neg(&line[i]))
-			s_map->map[s_map->rows][col++] = ft_atoi(&line[i]);
-		if (line[i] == '-')
-			i++;
-		while (ft_isdigit(line[i]))
-			i++;
-	}
-	s_map->rows++;
-	s_map->cols = col;
-}
-
-void	store_map(char *file, t_map *s_map)
-{
-	int fd;
-	char *line;
-
-	//fix to malloc correct amount of rows
-	s_map->map = (int**)malloc(sizeof(int*) * 11);
-	s_map->rows = 0;
-	s_map->cols = 0;
-	fd = open(file, O_RDONLY);
-	while (get_next_line(fd, &line) > 0)
-	{
-		store_map_line(line, s_map);
-		free(line);
-	}
-	close(fd);
-}
-
-void	print_map(t_map *s_map)
-{
-	int i;
-	int j;
-
-	i = 0;
-	while (i < s_map->rows)
-	{
-		j = 0;
-		while (j < s_map->cols)
-		{
-			ft_printf("%3d ", s_map->map[i][j++]);
-		}
-		ft_printf("\n");
-		i++;
-	}
+	mlx = (t_mlx*)malloc(sizeof(t_mlx));
+	mlx->init = mlx_init();
+	mlx->height = (s_map->rows * 60 < 2560) ;
+	mlx->width = s_map->cols * 60;
+	mlx->window = mlx_new_window(mlx->init, mlx->width, mlx->height, "Hello World!");
+	mlx->x = mlx->height/2;
+	mlx->y = mlx->width/2;
+	mlx_hook(mlx->window, 2, 0, check_key, (void*)mlx);
+	mlx_loop(mlx->init);
 }
 
 int		main(int argc, char **argv)
 {
-	t_mlx	*mlx;
 	t_map	*s_map;
 
 	if (argc != 2)
 		handle_error(1);
 	s_map = (t_map*)malloc(sizeof(*s_map));
 	store_map(argv[1], s_map);
-	print_map(s_map);
-	mlx = (t_mlx*)malloc(sizeof(t_mlx));
-	mlx->init = mlx_init();
-	mlx->height = 500;
-	mlx->width = 500;
-	mlx->window = mlx_new_window(mlx->init, mlx->height, mlx->width, "Hello World!");
-	mlx->x = mlx->height/2;
-	mlx->y = mlx->width/2;
-	mlx_hook(mlx->window, 2, 0, check_key, (void*)mlx);
-	mlx_loop(mlx->init);
+	print_map(s_map); //temporary to check if input has been stored correctly
+	handle_graphics(s_map);
 	return (0);
 }
---
+++ store_input.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   store_input.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/05 13:56:50 by sadawi            #+#    #+#             */
+/*   Updated: 2020/02/05 13:56:53 by sadawi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+void	store_map(char *file, t_map *s_map)
+{
+	int fd;
+	char *line;
+
+	//fix to malloc correct amount of rows
+	s_map->rows = 0;
+	s_map->cols = 0;
+	fd = open(file, O_RDONLY);
+	while (get_next_line(fd, &line) > 0)
+	{
+		malloc_extra_row(s_map);
+		store_map_line(line, s_map);
+		free(line);
+	}
+	close(fd);
+}
+
+void	store_map_line(char *line, t_map *s_map)
+{
+	int i;
+	int col;
+	int count;
+
+	i = 0;
+	col = 0;
+	check_line(line);
+	count = count_ints(line);
+	if (s_map->cols)
+		if (count != s_map->cols)
+			handle_error(2);
+	s_map->map[s_map->rows] = (int*)malloc(sizeof(int) * count);
+	while (line[i])
+	{
+		while (line[i] == ' ')
+			i++;
+		s_map->map[s_map->rows][col++] = ft_atoi(&line[i]);
+		while (line[i] != ' ' && line[i])
+			i++;
+	}
+	s_map->rows++;
+	s_map->cols = col;
+}
+
+void	malloc_extra_row(t_map *s_map)
+{
+	int **tmp;
+	int i;
+
+	tmp = (int**)malloc(sizeof(int*) * (s_map->rows));
+	ft_memcpy(tmp, s_map->map, sizeof(int**) * s_map->rows);
+	if (s_map->rows)
+		free(s_map->map);
+	s_map->map = (int**)malloc(sizeof(int*) * (s_map->rows + 1));
+	i = 0;
+	while (i < s_map->rows)
+	{
+		s_map->map[i] = tmp[i];
+		i++;
+	}
+	free(tmp);
+}
+
+void	check_line(char *line)
+{
+	int i;
+
+	i = 0;
+	while (line[i])
+	{
+		if (!ft_isdigit(line[i]) && line[i] != ' ' && !ft_isdigit_neg(&line[i]))
+			handle_error(2);
+		i++;
+	}
+}
+
+int		count_ints(char *line)
+{
+	int i;
+	int count;
+
+	i = 0;
+	count = 0;
+	while (line[i])
+	{
+		while (line[i] == ' ' && line[i])
+			i++;
+		if (ft_isdigit(line[i]) || ft_isdigit_neg(&line[i]))
+		{
+			if (line[i] == '-')
+				i++;
+			while (ft_isdigit(line[i]))
+				i++;
+			count++;
+		}
+	}
+	return (count);
+}
